10017a2a1f810671b3b5b632fa2768815c05fc4d
Death-Raider13 <lateefedidi4@gmail.com>
2025-12-04 13:56:48 +0100

    Add files via upload

---
+++ Number-swapping.py
@@ -0,0 +1,14 @@
+a= 5
+b = 39
+c= 1204
+
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+a, b, c = c, a, b
+print("After swapping:")
+print("a =", a) 
+print("b =", b)
+print("c =", c)
